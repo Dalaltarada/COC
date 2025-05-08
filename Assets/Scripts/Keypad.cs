6c5ce1d502fced371f32a1aa644ce9274f919f20
Dalaltarada <dalaltarada@gmail.com>
2025-05-09 01:56:24 +0300

    keypad works!

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    public GameObject player;         // Reference to the player GameObject
+    public GameObject keypadOB;       // Full keypad UI object
+
+    public Text textOB;               // Text display for entered digits
+    public string answer = "12345";   // Correct code
+
+    public AudioSource button;        // Sound on keypad press
+    public AudioSource correct;       // Sound on correct input
+    public AudioSource wrong;         // Sound on wrong input
+
+    void Start()
+    {
+        keypadOB.SetActive(false); // Hide keypad on start
+    }
+
+    public void Number(int number)
+    {
+        textOB.text += number.ToString();
+        button.Play();
+    }
+
+    public void Execute()
+    {
+        if (textOB.text == answer)
+        {
+            correct.Play();
+            textOB.text = "Success";
+        }
+        else
+        {
+            wrong.Play();
+            textOB.text = "Wrong";
+        }
+    }
+
+    public void Clear()
+    {
+        textOB.text = "";
+        button.Play();
+    }
+
+    public void Exit()
+    {
+        keypadOB.SetActive(false);
+        player.GetComponent<FirstPersonController>().canMove = true; // ✅ Re-enable movement
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (keypadOB.activeInHierarchy)
+        {
+            player.GetComponent<FirstPersonController>().canMove = false; // ⛔ Disable movement
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/KeypadTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class KeypadTrigger : MonoBehaviour
+{
+    public GameObject keypadOB;     // The full keypad UI
+    public GameObject keypadText;   // "Press E to interact" prompt
+    public GameObject player;       // Reference to the player
+
+    private bool inReach = false;
+
+    void Start()
+    {
+        keypadOB.SetActive(false);
+        keypadText.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inReach = true;
+            keypadText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inReach = false;
+            keypadText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inReach)
+        {
+            keypadOB.SetActive(true);
+            player.GetComponent<FirstPersonController>().canMove = false; // ⛔ Disable movement
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
